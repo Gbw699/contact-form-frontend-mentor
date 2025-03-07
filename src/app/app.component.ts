ed060f473db51bcc8e5209817147ab39b1c24cf1
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-03-07 11:42:57 -0300

    FEAT reactive form for contact submission; add validation and error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'contact-form-frontend-mentor';
+  formGroup = new FormGroup(
+    {
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      queryType: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+      consent: new FormControl(false, Validators.requiredTrue),
+    },
+    Validators.required
+  );
+
+  onSubmit() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+    }
+  }
 }
