c3992de7b487368b3a96d76de5371bcb9caa8002
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-03-14 12:30:08 -0300

    FEAT enhance contact form; add success dialog, improve radio button focus handling, and update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -21,14 +21,28 @@ export class AppComponent {
       email: new FormControl('', [Validators.required, Validators.email]),
       queryType: new FormControl('', Validators.required),
       message: new FormControl('', Validators.required),
-      consent: new FormControl(false, Validators.requiredTrue),
+      consent: new FormControl('', Validators.requiredTrue),
     },
     Validators.required
   );
+  dialogModal: any = viewChild<any>('succesDialog');
 
   onSubmit() {
     if (!this.formGroup.valid) {
       this.formGroup.markAllAsTouched();
+    } else {
+      this.dialogModal()?.nativeElement.showModal();
+      setTimeout(() => {
+        this.dialogModal()?.nativeElement.close();
+      }, 6000);
+    }
+
+    console.log(this.formGroup.value);
+  }
+
+  onRadioFocus() {
+    if (this.formGroup.controls.queryType.value === '') {
+      this.formGroup.controls.queryType.setValue('general-enquiry');
     }
   }
 }
